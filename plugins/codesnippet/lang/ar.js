8da59be3e07080ba7fd771772fd44ad974e50eb1
abumalick <abdullah.hilson@gmail.com>
2018-10-15 20:03:54 +0200

    initial commit

---
+++ plugins/codesnippet/lang/ar.js
@@ -0,0 +1,13 @@
+/**
+ * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.plugins.setLang('codesnippet', 'ar', {
+  button: 'أدمج قصاصة الكود',
+  codeContents: 'محتوى الكود',
+  emptySnippetError: 'قصاصة الكود لايمكن أن تكون فارغة.',
+  language: 'لغة البرمجة',
+  title: 'قصاصة الكود',
+  pathName: 'قصاصة الكود',
+});
